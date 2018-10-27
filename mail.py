016d9259d7b55f306515eb146a780d3a5935d2fe
Ujjwal Prakash <ujjwalprakash@Ujjwals-MacBook-Air.local>
2018-10-27 13:01:04 +0530

    edited mail.py

--- mail.py
+++ mail.py
@@ -1,17 +1,14 @@
 import smtplib
-
+import base64
 s = smtplib.SMTP('smtp.gmail.com', 587)
-
 s.starttls()
+s.login("sender_email_id","sender_email_password")
 
-s.login("sender_email_id", "sender_email_password")
-
-
-message = "I am at HashHacks"
-
-s.sendmail("sender_email_id", "receiver_email_id", message)
-
-s.quit()
-
-#Here we can use base64 for encrypting passwords
-                
+def send(message):
+    if(message):
+        s.sendmail("sender_email_id","receiver_email_id",message)
+        s.quit()
+    else:
+        message = "Sorry"
+        s.sendmail("sender_email_id","receiver_email_id",message)
+        s.quit()
